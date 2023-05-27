ac8793fe4ff2961cd249108072a5a93cd4931529
Felipe Begliomini <fnincao@hotmail.com>
2023-05-27 14:02:42 +0000

    1st version of UNET code

--- src/model/dataset.py
+++ src/model/dataset.py
@@ -1,5 +1,4 @@
 import os
-from PIL import Image
 from torch.utils.data import Dataset
 import numpy as np
 import rasterio
@@ -18,10 +17,22 @@ class PlanetDataset(Dataset):
     # Define len function
     def __len__(self):
         return len(self.images)
-    
+
     def __getitem__(self, index):
         img_path = os.path.join(self.image_dir, self.images[index])
         mask_path = os.path.join(self.mask_dir,
                                  self.images[index].replace("_planet.tif", "_ref.tif")) # noqa
-        
+        with rasterio.open(img_path) as ds:
+            ds_nir = ds.read(1)
+            ds_red = ds.read(2)
+            ds_green = ds.read(3)
+            image = np.stack([ds_nir, ds_red, ds_green], axis=-1)
+        with rasterio.open(mask_path) as ds:
+            mask = ds.read(1)
 
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations['image']
+            mask = augmentations['mask']
+
+        return image, mask
