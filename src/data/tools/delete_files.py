11664ca5c2de68feed752e5e331c8ad9997acecd
Felipe Begliomini <fnb25@cam.ac.uk>
2023-06-07 14:41:31 +0000

    Added module to delete images from data dir

---
+++ src/data/tools/delete_files.py
@@ -0,0 +1,34 @@
+'''Module to delete files from data folder'''
+
+import os
+
+# Specify the directory path
+directory ='../../../data/ai_data/saved_images'
+
+# Iterate over the files in the directory
+for filename in os.listdir(directory):
+    file_path = os.path.join(directory, filename)
+
+    # Check if the path is a file and matches the pattern
+    if os.path.isfile(file_path) and filename.endswith('.png'):
+        # Remove the file
+        os.remove(file_path)
+        
+        
+# Specify the directory path
+directories = ['../../../data/gee_data',
+             '../../../data/croped_data', 
+             '../../../data/ai_data/train_images',
+             '../../../data/ai_data/val_masks',
+             '../../../data/ai_data/train_masks',
+             '../../../data/ai_data/val_masks']
+
+for directory in directories:
+    # Iterate over the files in the directory
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+
+        # Check if the path is a file and matches the pattern
+        if os.path.isfile(file_path) and filename.endswith('.tif'):
+            # Remove the file
+            os.remove(file_path)
